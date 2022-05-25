4c309b26e0610c995a658b8f0a227bb7be790675
Sedat Kapanoglu <sedat@kapanoglu.com>
2022-05-24 22:40:36 -0700

    convert to single line

--- yello.asm
+++ yello.asm
@@ -1,22 +1,19 @@
         org 	#170
 
-        dw	#0013, #000A       ; 10
-        db	#BF, ' "'          ; PRINT "
-.text	db      'Hello world', 0   ; Hello world
-        dw	#0013, #0014       ; 20
-        db	#C5                ; REM
-
+        dw	#0022, #000A       ; 10
+        db	#BF, '"'           ; PRINT"
+.text	db      'Hello world"'
+        db      #01, #01, #C0      ; :'
 .start
         ; we do a POP BC here because our hello world
         ; text translates at some point to a PUSH BC
         ; the rest of the instructions are harmless
         ; allowing us to avoid a jumpblock at the 
         ; beginning of the program.
-        pop	bc                 
         ld	hl, text
 .loop
         ld	a, (hl)
-        or	a
+        cp      '"'
         ret	z
         call	#BB5A
         inc	hl
